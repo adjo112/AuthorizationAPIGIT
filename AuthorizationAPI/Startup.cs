2ce45abacd7bb7d26c15f7cbbdbb38e7f1b0153b
Adith Jose E A <53598518+Adithjose@users.noreply.github.com>
2022-05-20 18:15:35 +0530

    cors

--- AuthorizationAPI/Startup.cs
+++ AuthorizationAPI/Startup.cs
@@ -33,7 +33,15 @@ namespace AuthorizationAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            /*
+            services.AddCors(o => o.AddPolicy("authcors", builder =>
+            {
+                builder.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+            }));
+            */
+            
              services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll",
@@ -101,14 +109,16 @@ namespace AuthorizationAPI
 
             app.UseHttpsRedirection();
 
-
+            app.UseCors("AllowAll");
             app.UseRouting();
 
             app.UseAuthentication();
 
             app.UseAuthorization();
 
-			 app.UseCors("AllowAll");
+          //   app.UseCors("AllowAll");
+
+          //  app.UseCors("authcors");
 
             app.UseEndpoints(endpoints =>
             {
